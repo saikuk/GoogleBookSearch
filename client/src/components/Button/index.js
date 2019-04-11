bcfa1bf0701896e9a228f391e653539f971bf89e
Sai Kuk <kukcheong@gmail.com>
2019-04-10 22:23:36 -0400

    update

--- client/src/components/Button/index.js
+++ client/src/components/Button/index.js
@@ -3,7 +3,7 @@ import React from "react";
 // Destructuring the type, className, children and onClick props, applying them to the button element
 function Button(props) {
   return (
-    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn btn-success">
+    <button {...props} style={{ float: "right", marginBottom: 10 }} className="btn">
       {props.children}
     </button>
   );
